76f9ce7f3e0a424cc6a0be657f287d2303ee4950
breucker <breucker@ifris.org>
2015-04-24 01:36:57 +0200

    first functionnal core : add songbooks and songs :)

--- collections.js
+++ collections.js
@@ -5,6 +5,22 @@ Songs = new Meteor.Collection("songs");
 //Parties = new Meteor.Collection("parties");
 
 // server: populate collections with some initial documents
-Books.insert({name: "My first songbook"});
-var myBooks = Books.find({}).fetch();
-Songs.insert({name:"Stairway to heaven", artist:"Led Zepplin", tabs:"A B C D", book: myBooks[0]._id});
+// Books.insert({name: "Oldies goldies"});
+// Books.insert({name: "Just Dance"});
+// Books.insert({name: "Campfire"});
+// var myBooks = Books.find({}).fetch();
+
+// Songs.insert({name:"Stairway to heaven", artist:"Led Zepplin", tabs:"A B C D", songbook: myBooks[0]._id});
+// Songs.insert({name:"Stairway to heaven", artist:"Led Zepplin", tabs:"A B C D", songbook: myBooks[0]._id});
+// Songs.insert({name:"Stairway to heaven", artist:"Led Zepplin", tabs:"A B C D", songbook: myBooks[0]._id});
+
+
+// Songs.insert({name:"Daft Punk", artist:"Led Zepplin", tabs:"A B C D", songbook: myBooks[1]._id});
+// Songs.insert({name:"Daft Punk", artist:"Led Zepplin", tabs:"A B C D", songbook: myBooks[1]._id});
+// Songs.insert({name:"Daft Punk", artist:"Led Zepplin", tabs:"A B C D", songbook: myBooks[1]._id});
+
+
+// Songs.insert({name:"Will to live", artist:"Ben Harper", tabs:"A B B B", songbook: myBooks[2]._id});
+// Songs.insert({name:"Will to live", artist:"Ben Harper", tabs:"A B B B", songbook: myBooks[2]._id});
+// Songs.insert({name:"Will to live", artist:"Ben Harper", tabs:"A B B B", songbook: myBooks[2]._id});
+
--- songbooker.js
+++ songbooker.js
@@ -1,18 +1,55 @@
 if (Meteor.isClient) {
-  Meteor.subscribe("all-songs");
+ // Meteor.subscribe("all-songs");
 
-  //songbook
+  //songbook list
   Router.route('/', function () {
-    var songlist = Songs.find({}).fetch();
+    Template.songbooks.events({
+      'click #new-songbook': function () {
+        Books.insert({name: $("#songbook-name").val() });
+        $("#songbook-name").reset();
+      }
+    });
+
+    this.render('songbooks', {data: {songbooks: function(){
+        return Books.find({}).fetch();
+    }}});
+  });
+  //songbook
+  Router.route('/songbook/:_id', function () {
+    songlist = Songs.find({songbook: this.params._id}).fetch();
+    songbook = Books.findOne({_id: this.params._id});
     console.log('songlist :', songlist);
-    this.render('songbook', {data: {songlist: songlist}});
+
+    Template.songbook.events({
+      'click #new-song': function () {
+        Songs.insert({name: $("#song-name").val(),artist: $("#song-artist").val(), songbook: songbook._id });
+        $("#song-name").reset();
+        $("#song-artist").reset();
+      }
+    });
+
+    this.render('songbook', {data: {songlist: songlist, bookName: songbook.name}});
   });
 
   //song
   Router.route('/song/:_id', function () {
-    var song = Songs.findOne({_id: this.params._id});
-
+    song = Songs.findOne({_id: this.params._id});
+    _that = this;
     console.log("Displaying #"+ this.params._id );
+
+    Template.song.events({
+      'click #edit': function () {
+        _that.render('songEdit',{data:song});
+      }
+    });
+
+    Template.songEdit.events({
+      'click #save-tabs' : function(){
+        Songs.update(song._id, {$set:{tabs: $("#song-tabs").val()}});
+        _that.render('song', {data:song});
+      }
+    });
+
     this.render('song', {data:song});
   });
 }
